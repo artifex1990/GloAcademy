5088db4cdf4b0e29ca858fcd30e02de7680664d0
Pavel Demyanov <demyanovpaul@yandex.ru>
2023-12-19 09:17:09 +0300

    Заменена часть расчета на функциональную часть.

--- script.js
+++ script.js
@@ -1,36 +1,64 @@
 'use strict';
 
-const title = prompt('Как называется ваш проект?');
-const screens = prompt('Какие типы экранов нужно разработать?');
-const screenPrice = parseInt(prompt('Сколько будет стоить данная работа?'));
-const adaptive = confirm('Нужен ли адаптив на сайте?');
-const firstService = prompt('Какой дополнительный тип услуги нужен?');
-const firstServicePrice = parseInt(prompt('Сколько это будет стоить?'));
-const secondService = prompt('Какой дополнительный тип услуги нужен?');
-const secondServicePrice = parseInt(prompt('Сколько это будет стоить?'));
-const fullPrice = screenPrice + firstServicePrice + secondServicePrice;
-const rollback = Math.random() * 100;
-const servicePercentPrice = Math.ceil(fullPrice - rollback);
+let title = prompt('Как называется ваш проект?');
+let screens = prompt('Какие типы экранов нужно разработать?');
+let screenPrice = parseInt(prompt('Сколько будет стоить данная работа?'));
+let adaptive = confirm('Нужен ли адаптив на сайте?');
+let firstService = prompt('Какой дополнительный тип услуги нужен?');
+let firstServicePrice = parseInt(prompt('Сколько это будет стоить?'));
+let secondService = prompt('Какой дополнительный тип услуги нужен?');
+let secondServicePrice = parseInt(prompt('Сколько это будет стоить?'));
+let fullPrice = 0;
+let servicePercentPrice = 0;
 let discount = 0;
-let error = false;
+let allServicePrices = 0;
 
-switch(true) {
-    case fullPrice > 30000:
-        discount = fullPrice * 0.1;
-        console.log(`Ваша скидка 10% (${Math.ceil(discount * 100) / 100} руб.)!`);
-        break;
-    case fullPrice > 15000:
-        discount = fullPrice * 0.05;
-        console.log(`Ваша скидка 5% (${Math.ceil(discount * 100) / 100} руб.)!`);
-        break;
-    case fullPrice > 0:
-        console.log('Скидка не предусмотрена!');
-        break;
-    default:
-        error = true;
-        console.log('Что то пошло не так!');
+const getAllServicePrices = function(servicePrice1, servicePrice2) {
+    return servicePrice1 + servicePrice2;
 }
 
-if (!error) {
-    console.log(`Итоговая стоимость за вычетом отката посреднику и скидки: ${servicePercentPrice - discount} руб.`);
+function getFullPrice(screenPrice, call) {
+    return screenPrice + call(firstServicePrice, secondServicePrice);
 }
+
+function getTitle(title) {
+    let titleTemp = title;
+
+    if (!titleTemp) return titleTemp;
+
+    titleTemp = titleTemp.toLowerCase();
+
+    return titleTemp[0].toUpperCase() + titleTemp.slice(1);
+}
+
+function getServicePercentPrices(fullPrice) {
+    const rollback = Math.random() * 100;
+
+    return Math.ceil(fullPrice - rollback);
+}
+
+function getScreens(screens) {
+    return screens;
+}
+
+function getRollbackMessage(fullPrice) {
+    switch(true) {
+        case fullPrice > 30000:
+            return 'Ваша скидка 10%!';
+        case fullPrice > 15000:
+            return 'Ваша скидка 5%!';
+        case fullPrice > 0:
+            return 'Скидка не предусмотрена!';
+        default:
+            return 'Что то пошло не так!';
+    }
+}
+
+allServicePrices = getAllServicePrices(firstServicePrice, secondServicePrice);
+fullPrice = getFullPrice(screenPrice, getAllServicePrices);
+servicePercentPrice = getServicePercentPrices(fullPrice);
+
+console.log(`Название проекта '${getTitle(title)}'`);
+console.log(`Экраны для разработки ${getScreens(screens)}`);
+console.log(getRollbackMessage(fullPrice));
+console.log(`Итоговая стоимость за вычетом отката посреднику: ${servicePercentPrice} руб.`);
